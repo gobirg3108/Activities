17569942475d4b48c4b09ef6c377f03f2a9c78c0
gobirg3108 <gobigobi3108@gmail.com>
2024-07-04 23:04:45 +0530

    UPDATE

--- CRM-PROJECT/back-end/src/routes/followup.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendFollowUpEmail } = require('../controllers/emailController');
-const auth = require('../middlewares/auth');
-
-router.post('/send', auth, sendFollowUpEmail);
-
-module.exports = router;
--- CRM-PROJECT/back-end/src/routes/reports.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateReports } = require('../controllers/reportController');
-const auth = require('../middlewares/auth');
-
-router.get('/', auth, generateReports);
-
-module.exports = router;
