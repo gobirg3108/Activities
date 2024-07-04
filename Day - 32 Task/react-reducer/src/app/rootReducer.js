d4fc9c8ef6f9b372156bc4e54ad3e5ab7e978d94
gobirg3108 <gobigobi3108@gmail.com>
2024-07-04 22:58:58 +0530

    all new files updated

--- Day - 32 Task/react-reducer/src/app/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import cartReducer from '../features/cart/cartSlice';
-
-const rootReducer = combineReducers({
-  cart: cartReducer,
-  // Add more reducers as needed
-});
-
-export default rootReducer;
