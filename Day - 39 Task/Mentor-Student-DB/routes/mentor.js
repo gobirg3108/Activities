dc9dbae45d97bd79a166bfb69f592e3aea2f9af3
gobirg3108 <gobigobi3108@gmail.com>
2024-07-16 21:59:19 +0530

    update

--- Day - 39 Task/Mentor-Student-DB/routes/mentor.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const mentorController = require('../controllers/mentorController');
-
-router.post('/', mentorController.createMentor);
-router.post('/assign-students', mentorController.addStudentsToMentor);
-router.get('/:id/students', mentorController.getStudentsByMentor);
-
-module.exports = router;
